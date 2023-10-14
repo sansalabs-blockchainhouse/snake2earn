ca7f78f2591fef28392973ab2b2cd50ac8ddd4c9
Wesley Sousa <wesleysousa@MacBook-Air-de-Wesley.local>
2023-10-14 11:52:28 -0300

    fix: meta description

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+require("@solana/wallet-adapter-react-ui/styles.css");
+import type { AppProps } from "next/app";
+import dynamic from "next/dynamic";
+
+const WalletConnectionProvider = dynamic(
+  () => import("../providers/wallet-connection-provider"),
+  {
+    ssr: false,
+  }
+);
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <WalletConnectionProvider>
+      <Component {...pageProps} />
+    </WalletConnectionProvider>
+  );
 }
---
+++ src/providers/wallet-connection-provider.tsx
@@ -0,0 +1,48 @@
+import { ReactNode, useMemo } from 'react';
+
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import {
+  ConnectionProvider,
+  WalletProvider
+} from '@solana/wallet-adapter-react';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+import {
+  PhantomWalletAdapter,
+  SolflareWalletAdapter,
+  MathWalletAdapter
+} from '@solana/wallet-adapter-wallets';
+import { clusterApiUrl } from '@solana/web3.js';
+
+interface WalletConnectionProviderProps {
+  children: ReactNode;
+}
+
+function WalletConnectionProvider({ children }: WalletConnectionProviderProps) {
+  const network = WalletAdapterNetwork.Mainnet;
+
+  const endpoint = useMemo(
+    () =>
+      'https://fabled-magical-panorama.solana-mainnet.discover.quiknode.pro/2af224eaab7cf91c93d2aa1a62b0d8cea5b3d33e/' ||
+      clusterApiUrl(network),
+    [network]
+  );
+
+  const wallets = useMemo(
+    () => [
+      new PhantomWalletAdapter(),
+      new SolflareWalletAdapter(),
+      new MathWalletAdapter()
+    ],
+    []
+  );
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>{children}</WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+}
+
+export default WalletConnectionProvider;
